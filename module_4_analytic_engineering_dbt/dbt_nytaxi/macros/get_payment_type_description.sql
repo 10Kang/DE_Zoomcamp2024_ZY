0396315891f47922998650c0663b6548d460f5a8
Zhi Yong Kang <kangzhiyong1999@gmail.com>
2024-02-01 03:23:22 +0000

    Add: dbt_model_biquery

---
+++ module_4_analytic_engineering_dbt/dbt_nytaxi/macros/get_payment_type_description.sql
@@ -0,0 +1,19 @@
+ {#
+    This macro returns the description of the payment_type 
+#}
+
+{% macro get_payment_type_description(payment_type) -%}
+
+    case cast(replace({{ payment_type }}, '.0', '') as integer)
+        when 1 then 'Credit card'
+        when 2 then 'Cash'
+        when 3 then 'No charge'
+        when 4 then 'Dispute'
+        when 5 then 'Unknown'
+        when 6 then 'Voided trip'
+        else 'EMPTY'
+    end
+
+{%- endmacro %}
+
+              
