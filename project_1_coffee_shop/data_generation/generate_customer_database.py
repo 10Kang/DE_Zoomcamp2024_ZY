ad78c95fac44b4cff132d3c90aa161bdc192857c
Kang <kangzhiyong1999@gmail.com>
2024-02-29 22:54:01 +0800

    Update: generate_customer_database.py

--- project_1_coffee_shop/data_generation/generate_customer_database.py
+++ project_1_coffee_shop/data_generation/generate_customer_database.py
@@ -1,43 +1,88 @@
-import random
 from faker import Faker
-from faker.providers import phone_number
+import random
+from faker.providers.phone_number import Provider
+import pandas as pd
 
-fake = Faker()
-fake.add_provider(phone_number)
+from datetime import datetime
+import argparse
 
-def generate_customer():
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    gender = random.choice(['Male', 'Female'])
-    birthday = fake.date_of_birth(minimum_age=15, maximum_age=80)
-    email = fake.email()
-    phone_number = fake.phone_number()
-    customer_id = fake.random_number( )
+class MalaysiaPhoneNumberProvider(Provider):
+    """
+    A Provider for phone number.
+    """
 
-    return {
-        'ID':customer_id,
-        'First Name': first_name,
-        'Last Name': last_name,
-        'Gender': gender,
-        'Birthday': birthday,
-        'Email': email,
-        'Phone Number': phone_number
-    }
+    def malaysia_phone_number(self):
+        return f'+60 {self.msisdn()[4:]}'
 
-def generate_customer_database(num_customers):
-    customers = []
-    for _ in range(num_customers):
-        customers.append(generate_customer())
-    return customers
+def generate_customer_data(number,seed):
+    number = int(number)
+    seed = int(seed)
+    fake = Faker()
+    fake.add_provider(MalaysiaPhoneNumberProvider)
+    fake.seed_instance(seed)
+    random.seed(seed)
 
-def main():
-    num_customers = 10
-    customer_database = generate_customer_database(num_customers)
-    for idx, customer in enumerate(customer_database, start=1):
-        print(f"Customer {idx}:")
-        for field, value in customer.items():
-            print(f"{field}: {value}")
-        print()
+    customer_data = []
+    for i in range(number):
+        customer_id = fake.uuid4()
+        gender = random.choice(['Male','Female'])
+
+        if gender == 'Male':
+            first_name = fake.first_name_male()
+            last_name = fake.last_name_male()
+        else:
+            first_name = fake.first_name_female()
+            last_name = fake.last_name_female()
+            
+        birthday = fake.date_of_birth(minimum_age = 18, maximum_age=75)
+        email = fake.email()
+        phone_number = fake.malaysia_phone_number()
+
+        customer = {
+            'customer_id': customer_id,
+            'first_name': first_name,
+            'last_name': last_name,
+            'gender': gender,
+            'birthday': birthday,
+            'email': email,
+            'phone_no': phone_number
+        }
+        customer_data.append(customer)
+
+    df = pd.DataFrame(customer_data)
+
+    return df
+
+def main(params):
+    date_string = params.date_string
+    seed = params.seed
+    number = params.number
+    output = params.output
+    # get date as record to update new 
+    if date_string == None:
+        today_date = datetime.today()
+        date_string = today_date.strftime('%Y-%m-%d')
+    else:
+        date_string = params.date_string
+        
+    # filepath to save file 
+    filepath = f'{output}_customer_data_{date_string}.csv'
+
+    df = generate_customer_data(number,seed)
+
+    df.to_csv(filepath)
 
 if __name__ == "__main__":
-    main()
+    
+    parser = argparse.ArgumentParser(description='Generating customer database')
+    
+    # Add a positional argument
+    parser.add_argument('--output', help='Path to the output file',required=True)
+    parser.add_argument('--number', help='Number of new customer',required=True)
+    parser.add_argument('--seed', help='seed to ensure not replicate',required=True)
+    parser.add_argument('--date_string','-o', help='date of generating the new customer')
+    
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    
+    main(args)
